ac4d30a46cc92ba8305ee48ef22f9af23177f196
zyrif <rubaiyat9@gmail.com>
2019-09-29 03:45:48 +0600

    Programmatic route change logic, scroll and swipe logic added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <v-app>
     <navbar />
-    <v-content>
+    <v-content
+      v-touch:swipe="swipeHandler"
+    >
       <transition v-bind:name="transitionName">
         <router-view />
       </transition>
@@ -22,23 +24,78 @@ export default {
   components: {
     Navbar
   },
+  created () {
+    window.addEventListener('wheel', this.handleScroll)
+  },
+  destroyed () {
+    window.removeEventListener('wheel', this.handleScroll)
+  },
   watch: {
     $route (to, from) {
       const toOrder = to.meta.order
       const fromOrder = from.meta.order
       this.transitionName = toOrder < fromOrder ? 'slide-down' : 'slide-up'
     }
+  }, 
+  methods: {
+    // execute when scroll is detected
+    handleScroll (event) {
+      if (event.deltaY > 0) {
+        this.goToNextPage()
+      }
+      else if (event.deltaY < 0) {
+        this.goToPreviousPage()
+      }
+      else {
+        console.warn("mousewheel deltaY should not be zero")
+      }
+    },
+
+    // execute when touch is detected
+    swipeHandler (direction) {
+      if (direction == 'top') {
+        this.goToNextPage()
+      }
+      else if (direction == 'bottom') {
+        this.goToPreviousPage()
+      }
+      else {
+        //
+      }
+    },
+
+    // navigate functionality
+    goToNextPage () {
+      let routesObj = this.$router.options.routes
+      let totalRoutes = routesObj.length
+      // naming it nextOrder because it basically acts like one,
+      // since the meta order numbering starts at 1
+      let nextOrder = this.$route.meta.order
+
+      if (nextOrder < totalRoutes) {
+        this.$router.push(routesObj[nextOrder].path)
+      }
+
+    },
+    goToPreviousPage () {
+      let routesObj = this.$router.options.routes
+      // naming it nextOrder because it basically acts like one,
+      // since the meta order numbering starts at 1
+      let nextOrder = parseInt(this.$route.meta.order)
+
+      if (nextOrder > 1) {
+        this.$router.push(routesObj[nextOrder - 2].path)
+      }
+    }
   }
-  // beforeRouteUpdate (to, from, next) {
-  //   const toOrder = to.meta.order
-  //   const fromOrder = from.meta.order
-  //   console.log("toorder: " + toOrder + "fromorder: " + fromOrder)
-  //   this.transitionName = toOrder < fromOrder ? 'slide-down' : 'slide-up'
-  //   next()
-  // }
 }
 </script>
 
 <style lang="scss">
 @import 'src/assets/css/transitions';
+
+html {
+  overflow-y: hidden !important;
+}
+
 </style>
