445b1b462fe0c53b19c5c16c3bac5f2d6cbd14ae
zyrif <rubaiyat9@gmail.com>
2019-10-04 00:55:14 +0600

    Skills - layout added

---
+++ src/components/skills/SkillGrid.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-row>
+    <v-col cols="12">
+      <v-row align="center" v-if="!this.chip">
+        <v-card 
+          v-for="value in this.values" :key="value.name"
+          class="ml-3 mt-3"
+          flat
+          ripple
+          width="8vw"
+          height="8vw"
+        >
+          <v-card-title class="justify-center pa-1" style="font-size: 1vw; height: 100%">
+            <span>{{ value.name }}</span>
+          </v-card-title>
+        </v-card>
+      </v-row>
+      <v-row align="center" v-else>
+        <v-chip 
+          v-for="value in this.values" :key="value.name"
+          class="ma-2" 
+          label
+          ripple
+        >
+          {{ value.name }}
+        </v-chip>
+      </v-row>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    values: {
+      type: Array,
+      required: true
+    },
+    chip: {
+      type: Boolean
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -1,43 +1,61 @@
 <template>
   <transition name="fade" mode="out-in">
-    <v-container class="fill-height skills">
-        <div class="page">
-            <h1>Skills</h1>
-            <h3>Languages </h3>
-            <ul>
-              <li> Python </li>
-              <li> Javascript </li>
-              <li> C# </li>
-              <li> PHP </li>
-            </ul>
+    <v-container fluid class="fill-height skills">
+      <div class="page">
+        <h3>Languages </h3>
+        <skill-grid v-bind:values="this.languageList" v-bind:chip="$vuetify.breakpoint.mdAndDown" />
 
-            <h3>Services, Tools & Frameworks </h3>
-            <ul>
-              <li> Django </li>
-              <li> Vue.js </li>
-              <li> Vuetify </li>
-              <li> ASP.NET MVC </li>
-              <li> Wordpress </li>
-              <li> Node </li>
-              <li> Dlib </li>
-              <li> Keras </li>
-              <li> Tensorflow </li>
-              <li> Git </li>
-              <li> Github </li>
-              <li> Bitbucket </li>
-              <li> Visual Studio Code </li>
-              <li> Anaconda </li>
-              <li> Webpack </li>
-              <li> Babel </li>
-            </ul>
-        </div>
+        <h3> Frameworks, Tools and Services </h3>
+        <skill-grid v-bind:values="this.ftsCombinedList" v-bind:chip="$vuetify.breakpoint.mdAndDown" />
+      </div>
     </v-container>
   </transition>
 </template>
 
 <script>
+import SkillGrid from '@/components/skills/SkillGrid.vue'
 export default {
-    //
+  data () {
+    return {
+      languageList: [
+        {'name': 'Python', 'icon': 'icon', 'color': 'primary', 'link': '#'},
+        {'name': 'Javascript', 'icon': 'icon', 'color': 'primary', 'link': '#'},
+        {'name': 'C#', 'icon': 'icon', 'color': 'primary', 'link': '#'},
+        {'name': 'PHP', 'icon': 'icon', 'color': 'primary', 'link': '#'},
+      ], 
+      frameworksList: [
+        {'name': 'Django', 'icon': 'icon', 'color': 'primary', 'link': '#'},
+        {'name': 'Vue.js', 'icon': 'icon', 'color': 'primary', 'link': '#'},
+        {'name': 'Vuetify', 'icon': 'icon', 'color': 'primary', 'link': '#'},
+        {'name': 'ASP.NET MVC', 'icon': 'icon', 'color': 'primary', 'link': '#'},
+      ], 
+      toolsList: [
+        {'name': 'Git', 'icon': 'icon', 'color': 'primary', 'link': '#'},
+        {'name': 'Node', 'icon': 'icon', 'color': 'primary', 'link': '#'},
+        {'name': 'VS Code', 'icon': 'icon', 'color': 'primary', 'link': '#'},
+        {'name': 'Anaconda', 'icon': 'icon', 'color': 'primary', 'link': '#'},
+        {'name': 'Webpack', 'icon': 'icon', 'color': 'primary', 'link': '#'},
+        {'name': 'Babel', 'icon': 'icon', 'color': 'primary', 'link': '#'},
+        {'name': 'Dlib', 'icon': 'icon', 'color': 'primary', 'link': '#'},
+        {'name': 'Keras', 'icon': 'icon', 'color': 'primary', 'link': '#'},
+        {'name': 'Tensorflow', 'icon': 'icon', 'color': 'primary', 'link': '#'},
+      ],
+      servicesList: [
+        {'name': 'Github', 'icon': 'icon', 'color': 'primary', 'link': '#'},
+        {'name': 'Bitbucket', 'icon': 'icon', 'color': 'primary', 'link': '#'},
+        {'name': 'Firebase', 'icon': 'icon', 'color': 'primary', 'link': '#'},
+        {'name': 'Digital Ocean', 'icon': 'icon', 'color': 'primary', 'link': '#'},
+      ]
+    }
+  },
+  components: {
+    SkillGrid
+  },
+  computed: {
+    ftsCombinedList () {
+      return this.frameworksList.concat(this.toolsList, this.servicesList)
+    }
+  }
 }
 </script>
 
