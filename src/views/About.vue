e742989aaed21786aa7daa52cbcdb52bf3ddf42c
zyrif <rubaiyat9@gmail.com>
2019-10-08 19:21:14 +0600

    revert about styling

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,20 +2,11 @@
   <transition name="fade" mode="out-in">
     <v-container class="fill-height about">
       <div class="page">
-        <h1 class="display-2 mx-3 my-4">
-          <span class="blue--text">M</span>
-          <span class="font-weight-light mr-2">uhammad</span>
-          <span class="blue--text">R</span>
-          <span class="font-weight-light">ubaiyat</span>
-        </h1>
-        <div class="mx-3">
-          <p class="my-2">
-            <span>Mover of Bits</span>
-            <span class="blue--text mx-4 font-weight-black">|</span>
-            <span>Arranger of Pixels</span>
-          </p>
-          <p class="my-2">Likes to work with cutting-edge technologies to solve interesting problems</p>
-          <p class="my-2">Based on Dhaka / Bangladesh</p>
+        <h1>Muhammad Rubaiyat</h1>
+        <div>
+          <p> Mover of Bits | Arranger of Pixels </p>
+          <p> Likes to work with cutting-edge technologies to solve interesting problems </p>
+          <p> Based on Dhaka / Bangladesh </p>
         </div>
         <!-- <span> LinkedIn </span>
         <span> Github </span>
